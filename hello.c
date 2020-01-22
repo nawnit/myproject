98f10a3956dc8ecd3bc1900fd3f78a22e9750eb1
nawnit <nawnit86@gmail.com>
2020-01-22 16:20:06 +0000

    file updated

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
+int i = 100;
 printf("Hello world ! this is first file")
 printf("Forgot to mention my name.I'm Nawnit Kumar\n")
 printf("This is next push from local repository")
